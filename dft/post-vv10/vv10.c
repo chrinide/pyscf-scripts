770c2ebdbcf7e2b19d6b565ed7ef8a409c1ca465
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2018-08-10 21:13:53 +0200

    Update

--- dft/post-vv10/vv10.c
+++ dft/post-vv10/vv10.c
@@ -15,7 +15,7 @@ double vv10(const int n, const double coef_C,
   //const double coef_B = 5.9;
   const double const43 = 4.0/3.0*pi;
 
-	size_t idx, jdx;
+  size_t idx, jdx;
   double vv10_e = 0.0;
 
   double coef_beta;
@@ -23,43 +23,41 @@ double vv10(const int n, const double coef_C,
   double kappa_pref;
   kappa_pref = coef_B*(1.5*pi)/pow(9.0*pi,1.0/6.0);
 
-	for (idx=0; idx<n; idx++){
-		double point1x = coords[idx*3+0];
-		double point1y = coords[idx*3+1];
-		double point1z = coords[idx*3+2];
-		double rho1 = rho[idx];
+  for (idx=0; idx<n; idx++){
+    double point1x = coords[idx*3+0];
+    double point1y = coords[idx*3+1];
+    double point1z = coords[idx*3+2];
+    double rho1 = rho[idx];
     double weigth1 = weights[idx];
     double gamma1 = gnorm2[idx];
     double Wp1 = const43*rho1;
     double Wg1 = coef_C*pow(gamma1/(rho1*rho1),2);
     double W01 = sqrt(Wg1 + Wp1);
-		double kappa1 = pow(rho1,1.0/6.0)*kappa_pref;
-		double kernel = 0.0;
-#pragma omp parallel default(none) \
-        reduction(+:kernel) \
-        shared(point1x,point1y,point1z,coords,rho,weights,kappa_pref,gnorm2,kappa1,W01)
+    double kappa1 = pow(rho1,1.0/6.0)*kappa_pref;
+    double kernel = 0.0;
+#pragma omp parallel default(none) reduction(+:kernel) \
+    shared(point1x,point1y,point1z,coords,rho,weights,kappa_pref,gnorm2,kappa1,W01)
 {
 #pragma omp for nowait schedule(dynamic)
-		for (jdx=0; jdx<n; jdx++){
-			double R = (point1x-coords[jdx*3+0])*(point1x-coords[jdx*3+0]);
-			R += (point1y-coords[jdx*3+1])*(point1y-coords[jdx*3+1]);
-			R += (point1z-coords[jdx*3+2])*(point1z-coords[jdx*3+2]);
-		  double rho2 = rho[jdx];
-	    double weigth2 = weights[jdx];
+    for (jdx=0; jdx<n; jdx++){
+      double R = (point1x-coords[jdx*3+0])*(point1x-coords[jdx*3+0]);
+      R += (point1y-coords[jdx*3+1])*(point1y-coords[jdx*3+1]);
+      R += (point1z-coords[jdx*3+2])*(point1z-coords[jdx*3+2]);
+      double rho2 = rho[jdx];
+      double weigth2 = weights[jdx];
       double gamma2 = gnorm2[jdx];
-	    double Wp2 = const43*rho2;
+      double Wp2 = const43*rho2;
       double Wg2 = coef_C*pow(gamma2/(rho2*rho2),2);
       double W02 = sqrt(Wg2 + Wp2);
-		  double kappa2 = pow(rho2,1.0/6.0)*kappa_pref;
-		  double g = W01*R + kappa1;
-		  double gp = W02*R + kappa2;
+      double kappa2 = pow(rho2,1.0/6.0)*kappa_pref;
+      double g = W01*R + kappa1;
+      double gp = W02*R + kappa2;
       kernel += -1.5*weigth2*rho2/(g*gp*(g+gp));
-		}
+    }
 } 
-		vv10_e += weigth1*rho1*(coef_beta + 0.5*kernel);
-	}
+    vv10_e += weigth1*rho1*(coef_beta + 0.5*kernel);
+  }
  
-	//printf("VV10 Energy : %16.10f\n", vv10_e);
   return vv10_e;
 
 }
