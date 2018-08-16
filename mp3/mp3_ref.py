592396cdc7f579cd90a0095d91c489813b62fb64
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2018-08-16 17:31:33 +0200

    Update MP3

--- mp3/mp3_ref.py
+++ mp3/mp3_ref.py
@@ -29,7 +29,7 @@ ev = mf.mo_energy[ncore+nocc:]
 
 eri_mo = ao2mo.general(mf._eri, (c,c,c,c), compact=False)
 eri_mo = eri_mo.reshape(nocc+nvir,nocc+nvir,nocc+nvir,nocc+nvir)
-epsilon = 1.0/(eo.reshape(-1,1,1,1) + eo.reshape(-1,1,1) - ev.reshape(-1,1) - ev)
+epsilon = 1/(eo.reshape(-1,1,1,1) + eo.reshape(-1,1,1) - ev.reshape(-1,1) - ev)
 o = slice(0, nocc)
 v = slice(nocc, None)
 eri_mo = eri_mo.swapaxes(1, 2)
@@ -49,31 +49,30 @@ lib.logger.info(mf,"!**** E(HF+MP2): %12.8f" % (e_mp2+ehf))
 t = time.time()
 lib.logger.info(mf,"Starting MP3 energy")
 t2 = numpy.zeros((nocc,nocc,nvir,nvir))
-factor = numpy.einsum('abij,ijab->abij',eri_mo[v,v,o,o],epsilon)
 # Equation 1: 3rd order diagram 1
-t2 = 2.0*numpy.einsum('ruts,tsab,abru->abru', eri_mo[v,v,v,v], factor, epsilon) 
+t2 = 2.0*numpy.einsum('ruts,tsab,abru,abts->abru', eri_mo[v,v,v,v], eri_mo[v,v,o,o], epsilon, epsilon) 
 # Equation 2: 3rd order diagram 2 
-t2 += 2.0*numpy.einsum('cbad,rscb,adrs->adrs', eri_mo[o,o,o,o], factor, epsilon)
+t2 += 2.0*numpy.einsum('cbad,rscb,adrs,cbrs->adrs', eri_mo[o,o,o,o], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 3: 3rd order diagram 3
-t2 += -4.0*numpy.einsum('rbsc,stab,acrt->acrt', eri_mo[v,o,v,o], factor, epsilon)
+t2 += -4.0*numpy.einsum('rbsc,stab,acrt,abst->acrt', eri_mo[v,o,v,o], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 4: 3rd order diagram 4
-t2 += -4.0*numpy.einsum('rasb,stac,bcrt->bcrt', eri_mo[v,o,v,o], factor, epsilon)
+t2 += -4.0*numpy.einsum('rasb,stac,bcrt,acst->bcrt', eri_mo[v,o,v,o], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 5: 3rd order diagram 5
-t2 += 8.0*numpy.einsum('btsc,rsab,acrt->acrt', eri_mo[o,v,v,o], factor, epsilon)
+t2 += 8.0*numpy.einsum('btsc,rsab,acrt,abrs->acrt', eri_mo[o,v,v,o], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 6: 3rd order diagram 6
-t2 += 2.0*numpy.einsum('atsc,rsab,cbrt->cbrt', eri_mo[o,v,v,o], factor, epsilon)
+t2 += 2.0*numpy.einsum('atsc,rsab,cbrt,abrs->cbrt', eri_mo[o,v,v,o], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 7: 3rd order diagram 7
-t2 += -1.0*numpy.einsum('dbac,srdb,acrs->acrs', eri_mo[o,o,o,o], factor, epsilon)
+t2 += -1.0*numpy.einsum('dbac,srdb,acrs,dbrs->acrs', eri_mo[o,o,o,o], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 8: 3rd order diagram 8
-t2 += -1.0*numpy.einsum('trus,usab,abtr->abrt', eri_mo[v,v,v,v], factor, epsilon)
+t2 += -1.0*numpy.einsum('trus,usab,abtr,abus->abrt', eri_mo[v,v,v,v], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 9: 3rd order diagram 9
-t2 += 2.0*numpy.einsum('arbs,tsac,cbrt->bcrt', eri_mo[o,v,o,v], factor, epsilon)
+t2 += 2.0*numpy.einsum('arbs,tsac,cbrt,acst->bcrt', eri_mo[o,v,o,v], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 10: 3rd order diagram 10
-t2 += 2.0*numpy.einsum('rasb,stac,cbrt->cbrt', eri_mo[v,o,v,o], factor, epsilon)
+t2 += 2.0*numpy.einsum('rasb,stac,cbrt,acst->cbrt', eri_mo[v,o,v,o], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 11: 3rd order diagram 11
-t2 += -4.0*numpy.einsum('scat,rtbc,abrs->abrs', eri_mo[v,o,o,v], factor, epsilon)
+t2 += -4.0*numpy.einsum('scat,rtbc,abrs,cbrt->abrs', eri_mo[v,o,o,v], eri_mo[v,v,o,o], epsilon, epsilon)
 # Equation 12: 3rd order diagram 12
-t2 += -4.0*numpy.einsum('atsc,rsab,bctr->bcrt', eri_mo[o,v,v,o], factor, epsilon)
+t2 += -4.0*numpy.einsum('atsc,rsab,bctr,abrs->bcrt', eri_mo[o,v,v,o], eri_mo[v,v,o,o], epsilon, epsilon)
 t2 = t2.swapaxes(1,2)
 e_mp3 = numpy.einsum('iajb,iajb->', eri_mo[o,o,v,v].swapaxes(1,2), t2)
 lib.logger.info(mf,"!*** E(MP3): %12.8f" % e_mp3)
