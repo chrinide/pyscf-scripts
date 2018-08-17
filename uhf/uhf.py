4e725a938bc0564f4518236689b2f7de462f1ced
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2018-08-17 03:54:37 +0200

    Update

--- uhf/uhf.py
+++ uhf/uhf.py
@@ -2,24 +2,26 @@
 
 import numpy
 from pyscf import gto, scf, lib
-from pyscf.tools import wfn_format
+from pyscf.tools import molden
 
-name = 'no'
+name = 'h2o'
 
-mol = gto.M(
-    atom = '''N  0.0000  0.0000  0.0000;
-              O  0.0000  0.0000  1.1508''',
-    basis = '3-21g',
-    verbose = 4,
-    spin = 1
-)
+mol = gto.Mole()
+mol.basis = 'cc-pvdz'
+mol.atom = '''
+O
+H 1 1.1
+H 1 1.1 2 104
+'''
+mol.charge = 0
+mol.spin = 1
+mol.charge = 1
+mol.symmetry = 1
+mol.verbose = 4
+mol.build()
 
 mf = scf.UHF(mol)
 mf.kernel()
-
-lib.logger.info(mf, "UHF wave function properties")
-lib.logger.info(mf, 'S^2 = %.7f, 2S+1 = %.7f' % mf.spin_square())
-mf.dip_moment()
 
 coeffa = mf.mo_coeff[0][:,mf.mo_occ[0]>0]
 energya = mf.mo_energy[0][mf.mo_occ[0]>0]
@@ -29,10 +31,12 @@ coeffb = mf.mo_coeff[1][:,mf.mo_occ[1]>0]
 energyb = mf.mo_energy[1][mf.mo_occ[1]>0]
 occb = mf.mo_occ[1][mf.mo_occ[1]>0]
 
-with open(name+'.wfn', 'w') as f2:
-    wfn_format.write_mo(f2, mol, coeffa, mo_energy=energya, mo_occ=occa)
-    wfn_format.write_mo(f2, mol, coeffb, mo_energy=energyb, mo_occ=occb)
-    
+occ = numpy.hstack([occa,occb])
+coeff = numpy.hstack([coeffa,coeffb])
+with open(name+'.mol', 'w') as f2:
+    molden.header(mol, f2)
+    molden.orbital_coeff(mol, f2, coeff, occ=occ)
+
 norb = mf.mo_energy[0].size
 nmoa = energya.size
 nmob = energyb.size
@@ -75,37 +79,37 @@ fspt.write('CCIQA\n')
 fspt.write('La matriz D es:\n')
 for i in range(nmoa):
     for j in range(nmoa):
-        fspt.write('%i %i %.16f\n' % ((i+1), (j+1), rdm1a[i,j]))
+        fspt.write('%i %i %.4f\n' % ((i+1), (j+1), rdm1a[i,j]))
 for i in range(nmob):
     for j in range(nmob):
-        fspt.write('%i %i %.16f\n' % ((i+1+nmoa), (j+1+nmoa), rdm1b[i,j]))
+        fspt.write('%i %i %.4f\n' % ((i+1+nmoa), (j+1+nmoa), rdm1b[i,j]))
 fspt.write('La matriz d es:\n')
 for i in range(nmoa):
     for j in range(nmoa):
         for k in range(nmoa):
             for l in range(nmoa):
                 if (abs(rdm2a[i,j,k,l]) > 1e-12):
-                        fspt.write('%i %i %i %i %.10f\n' \
+                        fspt.write('%i %i %i %i %.4f\n' \
                         % ((i+1), (j+1), (k+1), (l+1), rdm2a[i,j,k,l]))
 for i in range(nmob):
     for j in range(nmob):
         for k in range(nmob):
             for l in range(nmob):
                 if (abs(rdm2b[i,j,k,l]) > 1e-12):
-                        fspt.write('%i %i %i %i %.10f\n' \
+                        fspt.write('%i %i %i %i %.4f\n' \
                         % ((i+1+nmoa), (j+1+nmoa), (k+1+nmoa), (l+1+nmoa), rdm2b[i,j,k,l]))
 for i in range(nmoa):
     for j in range(nmoa):
         for k in range(nmob):
             for l in range(nmob):
                 if (abs(rdm2ab[i,j,k,l]) > 1e-12):
-                        fspt.write('%i %i %i %i %.10f\n' \
+                        fspt.write('%i %i %i %i %.4f\n' \
                         % ((i+1), (j+1), (k+1+nmoa), (l+1+nmoa), rdm2ab[i,j,k,l]))
 for i in range(nmob):
     for j in range(nmob):
         for k in range(nmoa):
             for l in range(nmoa):
                 if (abs(rdm2ba[i,j,k,l]) > 1e-12):
-                        fspt.write('%i %i %i %i %.10f\n' \
+                        fspt.write('%i %i %i %i %.4f\n' \
                         % ((i+1+nmoa), (j+1+nmoa), (k+1), (l+1), rdm2ba[i,j,k,l]))
 fspt.close()                    
--- uhf/uhfno.py
+++ uhf/uhfno.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
 import numpy
-from pyscf import scf, gto, lib, lo, dft
+from pyscf import scf, gto, lib, lo, dft, ao2mo
 from pyscf.tools import molden
 
 mol = gto.Mole()
-mol.verbose = 4
+mol.basis = 'aug-cc-pvdz'
 mol.atom = '''
-O      0.000000      0.000000      0.118351
-H      0.000000      0.761187     -0.469725
-H      0.000000     -0.761187     -0.469725
+O
+H 1 1.1
+H 1 1.1 2 104
 '''
-mol.basis = 'aug-cc-pvtz'
-mol.charge = 1
+mol.charge = 0
 mol.spin = 1
+mol.charge = 1
+mol.symmetry = 1
+mol.verbose = 4
 mol.build()
 
 mf = dft.UKS(mol)
@@ -32,6 +34,10 @@ mo_coeff = mf.mo_coeff
 nb = mo_coeff[1].shape[1]
 s = mol.intor_symmetric("cint1e_ovlp_sph")
 dm = mf.make_rdm1()
+h1e = mf.get_hcore()
+e1 = numpy.einsum('ij,ji->', h1e, dm[0])
+e1 += numpy.einsum('ij,ji->', h1e, dm[1])
+lib.logger.info(mf,'H_core energy in AO basis %s' % e1)
 
 # Natural orbitals
 thresh = 1e-14
@@ -43,11 +49,32 @@ lib.logger.info(mf,'Idempotency of DM %s' % numpy.linalg.norm(dm.dot(dm)-dm))
 nocc, coeff = numpy.linalg.eigh(dm)
 nocc = 2*nocc
 nocc[abs(nocc)<thresh] = 0.0
+nocc[abs(nocc)>2.0-thresh] = 2.0
 coeff = numpy.dot(s12inv,coeff)
+dm = mf.make_rdm1(coeff, nocc)
+lib.logger.info(mf,'Idempotency of DM %s' % numpy.linalg.norm(dm.dot(dm)-dm))
 diff = reduce(numpy.dot,(coeff.T,s,coeff)) - numpy.identity(nb)
 lib.logger.info(mf,'Orthonormal %s' % numpy.linalg.norm(diff))
 index = numpy.argsort(-nocc)
 nocc = nocc[index]
 coeff = coeff[:,index]
-lib.logger.info(mf,'Natual occupancy %s' % nocc)
+lib.logger.info(mf,'Natural occupancy %s' % nocc[nocc>0])
 
+# Energy in NO basis
+nmo = nocc.shape[0]
+rdm1 = numpy.zeros((nmo,nmo))
+rdm1 = numpy.diag(nocc)
+h1e = mf.get_hcore()
+h1e = reduce(numpy.dot,(coeff.T,h1e,coeff))
+e1 = numpy.einsum('ij,ji', h1e, rdm1)
+lib.logger.info(mf,'H_core energy in NO basis %s' % e1)
+
+eri_ao = ao2mo.restore(1,mf._eri,nmo)
+eri_ao = eri_ao.reshape((nmo,nmo,nmo,nmo))
+# Now eri_ao is the ERI tensor in Mulliken (11,22)
+# To be consistent with promolden 
+# 1) Obtain 2-RDM
+# 2) Transform eri_ao -> eri_mo ao2mo.kernel(eri_ao,matrix)
+# 3) Contract e2 = numpy.einsum('pqrs,pqrs->', eri_mo, rdm2)*0.5
+# 4) energy = e1 + e2 + mol.energy_nuc()
+#lib.logger.info(mf,'Total energy in NO basis: %s' % energy)
