506b0a2690a99eb74712e1d20eea33f0307d2f3d
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2018-08-21 00:08:10 +0200

    Relativistic 2C GMP2

--- rel/x2c-gmp2.py
+++ rel/x2c-gmp2.py
@@ -24,24 +24,30 @@ pt2 = mp.MP2(mf)
 pt2.kernel()
 
 mf = x2c.UHF(mol)
-dm = mf.get_init_guess() + 0.0j
+dm = mf.get_init_guess() + 0.1j
 ex2c = mf.kernel(dm)
 
 lib.logger.info(mf,'**** Relativistic GMP2')
+oidx = mf.mo_occ>0
+vidx = mf.mo_occ<=0
 eri_ao = mol.intor('int2e_spinor')
 nao = eri_ao.shape[0]
 nocc = mol.nelectron
 nvir = nao - nocc
-co = mf.mo_coeff[:,:nocc]
-cv = mf.mo_coeff[:,nocc:]
-eri_mo = ao2mo.general(eri_ao,(co,cv,co,cv)).reshape(nocc,nvir,nocc,nvir)
+nmo = nocc + nvir
+co = mf.mo_coeff[:,oidx]
+cv = mf.mo_coeff[:,vidx]
+c = numpy.hstack((co,cv))
+o = slice(0, nocc)
+v = slice(nocc, None)
+eri_mo = ao2mo.general(eri_ao,(c,c,c,c)).reshape(nmo,nmo,nmo,nmo)
 eri_mo = eri_mo - eri_mo.transpose(0,3,2,1)
 eo = mf.mo_energy[:nocc]
 ev = mf.mo_energy[nocc:]
 e_denom = 1.0/(-ev.reshape(-1,1,1,1)+eo.reshape(-1,1,1)-ev.reshape(-1,1)+eo)
 t2 = numpy.zeros((nvir,nocc,nvir,nocc))
-t2 = eri_mo.transpose(1,0,3,2)*e_denom 
-e_mp2 = 0.25*numpy.einsum('iajb,aibj->', eri_mo, t2, optimize=True)
+t2 = eri_mo[v,o,v,o]*e_denom 
+e_mp2 = 0.25*numpy.einsum('iajb,aibj->', eri_mo[o,v,o,v], t2, optimize=True)
 lib.logger.info(mf,"!*** E(MP2): %s" % e_mp2)
 lib.logger.info(mf,"!*** E(X2C+MP2): %s" % (e_mp2+ex2c))
 
