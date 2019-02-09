bd553cac6e3bad39a05ae7a089bdcfca8092ca33
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2019-02-09 02:46:43 +0100

    Update ! A very simple X2C CI relativistic code !

--- rel/todo/slater.py
+++ rel/todo/slater.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-import numpy
-import math
-from itertools import combinations
+from functools import reduce
+import numpy, math
 
 from pyscf import scf, lib, ao2mo
-from pyscf.lib import logger
 
 def find1(s):
     return [i for i,x in enumerate(bin(s)[2:][::-1]) if x is '1']
@@ -15,25 +13,21 @@ def str2orblst(string, norb):
     vir = []
     occ.extend([x for x in find1(string)])
     for i in range(norb): 
-        if not (string & numpy.uint64(1<<i)):
+        if not (string & 1<<i):
             vir.append(i)
     return occ, vir
+
+def num_strings(n, m):
+    if m < 0 or m > n:
+        return 0
+    else:
+        return math.factorial(n) // (math.factorial(n-m)*math.factorial(m))
 
 def orblst2str(lst):
     string = numpy.uint64(0)
     for idx in lst:
         string ^= numpy.uint64(1<<idx)
     return string
-
-def addorb(string, idx):
-    string0 = string
-    string0 |= numpy.uint64(1<<idx)
-    return string0
-
-def rmorb(string, idx):
-    string0 = string
-    string0 &= ~numpy.uint64((1<<idx))
-    return string0
 
 def str_diff(string0, string1):
     des_string0 = []
@@ -43,22 +37,16 @@ def str_diff(string0, string1):
     cre_string0.extend([x for x in find1(df & string1)])
     return des_string0, cre_string0    
 
-def num_strings(n, m):
-    if m < 0 or m > n:
-        return 0
-    else:
-        return math.factorial(n) // (math.factorial(n-m)*math.factorial(m))
-
 def cre_des_sign(p, q, string0):
     if p == q:
         return 1
     else:
-        if (string0 & numpy.uint64(1<<p)) or (not (string0 & numpy.uint64(1<<q))):
+        if (string0 & (1<<p)) or (not (string0 & (1<<q))):
             return 0
         elif p > q:
-            mask = numpy.uint64(1 << p) - numpy.uint64(1 << (q+1))
+            mask = (1 << p) - (1 << (q+1))
         else:
-            mask = numpy.uint64(1 << q) - numpy.uint64(1 << (p+1))
+            mask = (1 << q) - (1 << (p+1))
         return (-1) ** bin(string0 & mask).count('1')
 
 def print_dets(self,strs):
@@ -69,47 +57,53 @@ def print_dets(self,strs):
         logger.info(self,'Det %d %s' % (i,bin(strs[i])))
     return self
 
-def gen_cisd(self):
+def make_strings(self,orb_list):
+    '''Generate string from the given orbital list.
+
+    Returns:
+        list of int64.  One int64 element represents one string in binary format.
+        The binary format takes the convention that the one bit stands for one
+        orbital, bit-1 means occupied and bit-0 means unoccupied.  The lowest
+        (right-most) bit corresponds to the lowest orbital in the orb_list.
+
+    Exampels:
+
+    >>> [bin(x) for x in make_strings((0,1,2,3),2)]
+    [0b11, 0b101, 0b110, 0b1001, 0b1010, 0b1100]
+    >>> [bin(x) for x in make_strings((3,1,0,2),2)]
+    [0b1010, 0b1001, 0b11, 0b1100, 0b110, 0b101]
+    '''
+    orb_list = list(orb_list)
     nelec = self.mol.nelectron
-    nocc = self.mol.nelectron
-    norb = self.mo_coeff.shape[1]
-    nvir = norb - self.mol.nelectron
-    hf_str = int('1'*nelec, 2)
-
-    ndet_s = nocc*nvir
-    ndet_d = num_strings(nocc,2) * num_strings(nvir,2)
-    ndets = ndet_s + ndet_d + 1
-
-    logger.info(self, 'Number of determinants: %s', ndets)
-    strs = numpy.empty((ndets), dtype=numpy.uint64)
-    strs[0] = hf_str
-
-    kk = 1
-    occ, vir = str2orblst(strs[0], norb)
-
-    for i in occ:
-        for j in vir:
-            stra = rmorb(strs[0], i)
-            stra = addorb(stra, j)
-            strs[kk] = stra
-            kk += 1
-    
-    for i1, i2 in combinations(occ, 2):
-        for j1, j2 in combinations(vir, 2):
-            stra = rmorb(strs[0], i1)
-            stra = addorb(stra, i2)
-            stra = rmorb(stra, j1)
-            stra = addorb(stra, j2)
-            strs[kk] = stra
-            kk += 1
-
-    return strs
+    assert(nelec >= 0)
+    if nelec == 0:
+        return numpy.asarray([0], dtype=numpy.int64)
+    elif nelec > len(orb_list):
+        return numpy.asarray([], dtype=numpy.int64)
+    def gen_str_iter(orb_list, nelec):
+        if nelec == 1:
+            res = [(1<<i) for i in orb_list]
+        elif nelec >= len(orb_list):
+            n = 0
+            for i in orb_list:
+                n = n | (1<<i)
+            res = [n]
+        else:
+            restorb = orb_list[:-1]
+            thisorb = 1 << orb_list[-1]
+            res = gen_str_iter(restorb, nelec)
+            for n in gen_str_iter(restorb, nelec-1):
+                res.append(n | thisorb)
+        return res
+    strings = gen_str_iter(orb_list, nelec)
+    assert(strings.__len__() == num_strings(len(orb_list),nelec))
+    return numpy.asarray(strings, dtype=numpy.int64)
 
 def make_hdiag(self,h1e,h2e,h,strs):
     ndets = strs.shape[0]
     norb = h1e.shape[0]
-    diagj = numpy.einsum('iijj->ij', h2e)
-    diagk = numpy.einsum('ijji->ij', h2e)
+    diagj = lib.einsum('iijj->ij', h2e)
+    diagk = lib.einsum('ijji->ij', h2e)
     for i in range(ndets):
         stra = strs[i]
         occs = str2orblst(stra, norb)[0]
@@ -147,9 +141,6 @@ def make_hoffdiag(self,h1e,h2e,h,strs):
                     v = h2e[a,i,b,j] - h2e[a,j,b,i]
                     sign = cre_des_sign(b, j, stri)
                     sign*= cre_des_sign(a, i, stri)
-                #v = h2e[a,i,b,j] - h2e[a,j,b,i]
-                #sign = cre_des_sign(b, j, stri)
-                #sign*= cre_des_sign(a, i, stri)
                 h[ip,jp] = sign * v
                 h[jp,ip] = h[ip,jp].conj()
             else:
@@ -160,7 +151,7 @@ if __name__ == '__main__':
     from pyscf import gto, scf, x2c, ao2mo
     from pyscf.tools.dump_mat import dump_tri
     mol = gto.Mole()
-    mol.basis = 'sto-6g'
+    mol.basis = 'cc-pvdz'
     mol.atom = '''
     H 0.0000  0.0000  0.0000
     H 0.0000  0.0000  0.7500
@@ -171,7 +162,7 @@ if __name__ == '__main__':
     mol.symmetry = 0
     mol.build()
 
-    mf = x2c.UHF(mol)
+    mf = x2c.RHF(mol)
     mf.kernel()
 
     e_core = mol.energy_nuc() 
@@ -182,17 +173,19 @@ if __name__ == '__main__':
     h1e = reduce(numpy.dot, (mf.mo_coeff[:,:nmo].conj().T, \
     mf.get_hcore(), mf.mo_coeff[:,:nmo]))
 
-    strs = gen_cisd(mf)
+    orb_list = list(range(nmo))
+    strs = make_strings(mf,orb_list) 
     ndets = strs.shape[0]
+    lib.logger.info(mf, 'Number of dets in civec %s', ndets)
     h = numpy.zeros((ndets,ndets), dtype=numpy.complex128)
     h = make_hdiag(mf,h1e,eri_mo,h,strs) 
     h = make_hoffdiag(mf,h1e,eri_mo,h,strs)
-    dump_tri(mf.stdout,h,ncol=15,digits=4)
+    #dump_tri(mf.stdout,h,ncol=15,digits=4)
 
     e,c = numpy.linalg.eigh(h)
     e += e_core
-    logger.info(mf, 'Ground state energy %s', e[0])
-    logger.info(mf, 'Ground state civec %s', c[:,0])
+    lib.logger.info(mf, 'Ground state energy %s', e[0])
+    #lib.logger.info(mf, 'Ground state civec %s', c[:,0])
     norm = numpy.einsum('i,i->',c[:,0].conj(),c[:,0])
-    logger.info(mf, 'Norm of ground state civec %s', norm)
+    lib.logger.info(mf, 'Norm of ground state civec %s', norm)
 
