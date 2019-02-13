6d322296fce5916f965dcc1c0a5531471876b500
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2019-02-13 01:13:49 +0100

    Update

--- rel/slater/ci.h
+++ rel/slater/ci.h
@@ -20,6 +20,7 @@ double compute_cre_des_sign(int a, int i, uint64_t *stria);
 int *compute_occ_list(uint64_t *string, int norb, int nelec);
 int *get_single_excitation(uint64_t *str1, uint64_t *str2);
 int *get_double_excitation(uint64_t *str1, uint64_t *str2);
+int *get_triple_excitation(uint64_t *str1, uint64_t *str2);
 
 //int popcount(uint64_t bb);
 //int trailz(uint64_t v);
